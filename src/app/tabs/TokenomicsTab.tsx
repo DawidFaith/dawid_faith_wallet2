c102db9de867015c6e99c1c4664974e6478f761d
DawidFaith <dawid.faith@gmail.com>
2025-07-29 11:05:00 +0000

    fix(TokenomicsTab): korrigiere Schreibfehler im Text "Tokenomics Bereich"

--- src/app/tabs/TokenomicsTab.tsx
+++ src/app/tabs/TokenomicsTab.tsx
@@ -1,5 +1,5 @@
 export default function TokenomicsTab() {
   return (
-    <div className="text-white text-xl">Tokenomics Bereiche</div>
+    <div className="text-white text-xl">Tokenomics Bereich</div>
   );
 }
